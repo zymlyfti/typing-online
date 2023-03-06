87b4ada0e193549e337cf3bda7d77a3ae1268028
zymltfti <ir255trs@gmail.com>
2023-03-06 21:51:43 +0900

    First Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const mysql = require('mysql2/promise');
+
+const port = 3000
+
+//template engine
+app.set('view engine', 'ejs');
+
+//静的ファイルを宣言
+app.use(express.static('public'));
+
+
+//ルーティング-------------------------
+
+//index
+app.get('/',(req,res)=>{
+    const ipaddr = req.connection.remoteAddress;
+
+    const txt = 'test';
+    res.render('index',{sdata1:txt});
+});
+
+//クライアントでio()の実行、すなわちsocket.ioサーバーに接続された場合に発火
+io.on('connection',(socket)=>{
+    console.log('ユーザーが接続しました');
+
+
+    //接続が終了されたら
+    socket.on('disconnect', function(data) {
+        console.log('ユーザーが退室したした');
+    });
+});
+
+http.listen(port,function() {
+    console.log('listening on 3000');
+});
---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+//全てのDOMの読み込みが終わってから処理を開始
+$(document).ready(function() {
+
+const socket = io();
+
+});
